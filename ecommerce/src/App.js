1653648c945942cfa163fdd5adb1c8db5c3061fc
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-05-11 19:08:43 +0500

    create-react-app-done

---
+++ ecommerce/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
